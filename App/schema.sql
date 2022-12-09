a04beab7d454d42f78bc47a0d9cdd7ada8ebc20f
Jane-912 <120090564@link.cuhk.edu.cn>
2022-12-09 21:22:19 +0800

    12.9

--- App/schema.sql
+++ App/schema.sql
@@ -6,17 +6,16 @@ USE Chip;
 DROP TABLE IF EXISTS Consumer;
 DROP TABLE IF EXISTS Plant_owner;
 DROP TABLE IF EXISTS Own;
-DROP TABLE IF EXISTS Machine_type;
 DROP TABLE IF EXISTS Machine;
 DROP TABLE IF EXISTS Operation_machine_cost;
 DROP TABLE IF EXISTS Packages;
-DROP TABLE IF EXISTS Handler_record;
 DROP TABLE IF EXISTS Process_record;
 DROP TABLE IF EXISTS Chip_requires_operation;
 
 CREATE TABLE Consumer(
     consumer_id varchar(20) NOT NULL,
     password varchar(50) NOT NULL,
+    balance float(10,2) DEFAULT 0.00,
     PRIMARY KEY (consumer_id)
 );
 CREATE TABLE Plant_owner(
@@ -27,22 +26,18 @@ CREATE TABLE Plant_owner(
 CREATE TABLE Own(
     plant_id varchar(50) NOT NULL,
     owner_id varchar(20) NOT NULL,
-    type_name varchar(50) NOT NULL,
     income float(8,2),
     PRIMARY KEY (plant_id, owner_id)
     -- FOREIGN KEY (type_name) REFERENCES Machine_type(type_name),
     -- FOREIGN KEY (owner_id) REFERENCES Plant_owner(owner_id)
 );
-CREATE TABLE Machine_type(
-    type_name varchar(50) NOT NULL,
-    quota int NOT NULL,
-    PRIMARY KEY (type_name)
-);
+
 CREATE TABLE Machine(
     machine_id varchar(50) NOT NULL,
     plant_id varchar(50) NOT NULL,
     operation_type varchar(20) NOT NULL,
     status varchar(10),
+    quota int NOT NULL,
     PRIMARY KEY (machine_id)
     -- FOREIGN KEY (plant_id) REFERENCES Own(plant_id)
 );
@@ -63,23 +58,18 @@ CREATE TABLE Packages(
     -- FOREIGN KEY (plant_id) REFERENCES Own(plant_id),
     -- FOREIGN KEY (consumer_id) REFERENCES Consumer(consumer_id)
 );
-CREATE TABLE Handler_record(
-    package_id varchar(50) NOT NULL,
-    start_time TIMESTAMP NOT NULL DEFAULT now(),
-    end_time TIMESTAMP NOT NULL,
-    expense float(8,2) NOT NULL,
-    plant_id varchar(50) NOT NULL,
-    PRIMARY KEY (package_id)
-    -- FOREIGN KEY (plant_id) REFERENCES Own(plant_id)
-);
+
 CREATE TABLE Process_record(
     package_id varchar(50) NOT NULL,
     operation_type varchar(20) NOT NULL,
     machine_id varchar(50) NOT NULL,
     start_time TIMESTAMP NOT NULL DEFAULT now(),
     end_time TIMESTAMP NOT NULL,
+    plant_id varchar(50) NOT NULL,
     expense float(8,2) NOT NULL,
+    status varchar(10),
     PRIMARY KEY (package_id, operation_type, machine_id)
+    -- FOREIGN KEY (plant_id) REFERENCES Own(plant_id)
 );
 CREATE TABLE Chip_requires_operation(
     chip_type varchar(20) NOT NULL,
