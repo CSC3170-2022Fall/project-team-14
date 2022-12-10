3a949d1bbbdefca3ee18166121a99a02aec9b17e
QinLan18 <118010246@link.cuhk.edu.cn>
2022-12-10 22:55:41 +0800

    new commit

--- App/app.py
+++ App/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config.from_mapping(
         SECRET_KEY='dev',
     )
-db.init_db()
+db.get_db()
 app.register_blueprint(auth.bp)
 app.register_blueprint(plant.bp)
 app.register_blueprint(consumer.bp)
