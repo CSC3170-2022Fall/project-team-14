5b48638b4d1045e5f6b56e023fd221e525e6e515
张梦瑶 <120090171@link.cuhk.edu.cn>
2022-12-11 22:23:43 +0800

    modify plant.py

--- App/plant.py
+++ App/plant.py
@@ -12,38 +12,53 @@ bp = Blueprint('plant', __name__)
 
 @bp.route('/index_plant')
 def index_plant():
-    return render_template('index_plant.html')
+    if(g.user):
+        db = get_db()
+        error = None
+        cursor = db.cursor()
+        package_list=[]
+        temp =[]
+        cursor.execute("SELECT package_id, chip_type, chip_number, consumer_id FROM Packages WHERE plant_id = %s",g.user)
+        package_list_1 = cursor.fetchall()
+        print(package_list_1[0][0])
+        cursor.execute("SELECT start_time, status FROM Process_record WHERE package_id = %s", str(package_list_1[0][0]))
+        package_list_2 = cursor.fetchall()
+        print(package_list_1[0])
+        print(package_list_2)
+        # for i in range(0,4):
+        #     package_list.append(package_list_1[0][i])
+        # for j in range(0,2):
+        #     package_list.append(package_list_2[0][j])
+        package_list_11=package_list_1[0]+package_list_2[0]
+        package_list.append(package_list_11)
+        print(package_list)
+        cursor.execute("SELECT machine_id, status, start_time, end_time FROM Process_record WHERE plant_id = 333")
+        machine_list = cursor.fetchall()
+        print(machine_list)
+        return render_template('index_plant.html', package_list=package_list, machine_list=machine_list)
 
 
-@bp.route('/index_plant', methods=('GET', 'POST'))
-def packagelist():
-    if (g.user):
-        if request.method == 'POST':
-            plant_id = request.form["plant_id"]
-            db = get_db()
-            error = None
-            cursor = db.cursor()
-            packagelist=[]
-            cursor.execute("SELECT package_id, chip_type, chip_number, customer_id FROM Packages WHERE plant_id = %s", plant_id)
-            package_list_1 = cursor.fetchall()
-            packagelist.append(package_list_1)
-            cursor.execute("SELECT start_time, status FROM Process_record WHERE package_id = SELECT package_id FROM Packages WHERE plant_id = %s", plant_id)
-            package_list_2 = cursor.fetchall()
-            packagelist.append(package_list_2)
-
+# @bp.route('/index_plant', methods=('GET', 'POST'))
+# def packagelist():
+#     # if (g.user):
+#     #     if request.method == 'POST':
+#             #plant_id = request.form["plant_id"]
+#             db = get_db()
+#             error = None
+#             cursor = db.cursor()
+#             cursor.execute("SELECT package_id, chip_type, chip_number, consumer_id FROM Packages WHERE plant_id = 333")
+#             package_list_1 = cursor.fetchall()
+#             cursor.execute("SELECT start_time, status FROM Process_record WHERE package_id = SELECT package_id FROM Packages WHERE plant_id = 333")
+#             package_list_2 = cursor.fetchall()
+#             package_list=package_list_1+package_list_2
+#             cursor.execute("SELECT machine_id, status, start_time, end_time FROM Process_record WHERE plant_id = 333")
+#             machine_list = cursor.fetchall()
+#             print(machine_list)
             
 
-            machine_id = request.form["machine_id"]
-            db = get_db()
-            error = None
-            cursor = db.cursor()
-            cursor.execute("SELECT machine_id, status, start_time, end_time FROM Process_record WHERE machine_id = %s", (machine_id))
-            machine_list = cursor.fetchone()
-            
 
 
-
-            return render_template('/index_plant.html', package_list_1=package_list_1,package_list_2=package_list_2, machine_list=machine_list)
+#             return render_template('/index_plant.html', package_list=package_list, machine_list=machine_list)
 
 
 
@@ -53,9 +68,6 @@ def change_start_operation():
         if request.method == 'POST':
             db = get_db()
             cursor = db.cursor()
-            cursor.execute(
-                            "INSERT INTO Process_record(package_id,operation_type,machine_id,start_time,end_time,plant_id,status) VALUES (%s,%s,%s,%s,%s,%s,%s)",(2,"2",2,2,2,3,"2")
-                            ) 
             machine_id=request.form['machine_id']
             start_time = request.form["start_time"]
             operation_type=request.form["operation_type"]
