6d54b8e028678197d0f01511d2f38d9ee140925b
Jane-912 <114859390+Jane-912@users.noreply.github.com>
2022-12-12 09:36:38 +0800

    search按钮更新页面未实现，完成下单及支付

--- App/consumer.py
+++ App/consumer.py
@@ -11,9 +11,24 @@ bp = Blueprint('consumer', __name__)
 @bp.route('/index_consumer')
 def index_consumer():
     if(g.user):
-        return render_template('index_consumer.html')
-    return redirect(url_for('auth.login'))
-    # return render_template('index_consumer.html')
+        db = get_db()
+        cursor = db.cursor()
+        chip_type = []
+        chips = cursor.execute("SELECT DISTINCT chip_type FROM Chip_expense")
+        for i in range(chips):
+            ttt = cursor.fetchone()
+            chip_type.append(ttt)
+        plant_id = []
+        plants = cursor.execute("SELECT DISTINCT plant_id FROM Machine WHERE status = %s ORDER BY plant_id ASC", "IDLE")
+        for i in range(plants):
+            ttt = cursor.fetchone()
+            plant_id.append(ttt)
+        # display package list
+        cursor.execute("SELECT package_id, chip_type, chip_number, plant_id, price FROM Packages WHERE consumer_id = %s", g.user)
+        package_list = cursor.fetchall()
+        return render_template('index_consumer.html',  package_list = package_list, chip_type = chip_type, plant_id = plant_id)
+    # return redirect(url_for('auth.login'))
+    return render_template('index_consumer.html')
 
 @bp.route('/logout')
 def logout():
@@ -21,44 +36,41 @@ def logout():
     return redirect(url_for('auth.login'))
 
 
-@bp.route('/')
-def packagelist():
+@bp.route('/searchpackage', methods=('GET', 'POST'))
+def searchpackage():
     if (g.user):
-        if request.method == 'POST':
-            package_id = request.form.get("package_id")
-            db = get_db()
-            error = None
-            cursor = db.cursor()
-            cursor.execute("SELECT package_id, chip_type, chip_number, price FROM Packages WHERE package_id = %s", (package_id))
-            if cursor.fetchone() is not None:
-                error = 'Package {} does not exist.'.format(package_id)
-                return render_template('consumer/packagelist.html', error = error)
-            else:
-                list1 = cursor.fetchall()
-                info1 = [i for i in range(len(list1))]
-                cursor.execute("SELECT package_id, start_time, status FROM Process_record WHERE package_id = %s", package_id)
-                list2 = cursor.fetchall()
-                info2 = [i for i in range(len(list2))]
-                return render_template('consumer/packagelist.html', info1 = info1, info2 = info2)
+        package_list = []
+        db = get_db()
+        cursor = db.cursor()
+        chip_type = []
+        chips = cursor.execute("SELECT DISTINCT chip_type FROM Chip_expense")
+        for i in range(chips):
+            ttt = cursor.fetchone()
+            chip_type.append(ttt)
+        plant_id = []
+        plants = cursor.execute("SELECT DISTINCT plant_id FROM Machine WHERE status = %s ORDER BY plant_id ASC", "IDLE")
+        for i in range(plants):
+            ttt = cursor.fetchone()
+            plant_id.append(ttt)
+        packages = cursor.execute("SELECT package_id FROM Packages WHERE consumer_id = %s", g.user)
+        for i in range(packages):
+            ttt = cursor.fetchone()
+            package_list.append(ttt)
+        if request.method == 'POST':           
+            package_id = request.form.get('package_id')
+            cursor.execute("SELECT package_id, start_time, status FROM Process_record WHERE package_id = %s", package_id)
+            package_id_list = cursor.fetchall()
+        return render_template('index_consumer.html', package_list = package_list, chip_type = chip_type, plant_id = plant_id, package_id_list = package_id_list)
 
 @bp.route('/registerpackage', methods=('GET', 'POST'))
 @login_required
 def registerpackage():
     if (g.user):
+        # get all chip types and available plants
         if request.method == 'POST':           
             package_id = generate_packageid()
             db = get_db()
             cursor = db.cursor()
-
-            chip_type_list = []
-            chips = cursor.execute("SELECT * FROM Chip_expense").fetchall()
-            for i in range(len(chips)):
-                chip_type_list.append(i)
-            plant_id_list = []
-            plants = cursor.execute("SELECT * FROM OWn").fetchall()
-            for i in range(len(plants)):
-                plant_id_list.append(i)
-
             chip_type =  request.form.get('chip_type')
             chip_number = request.form.get('chip_number')
             plant_id = request.form.get('plant_id')
@@ -69,44 +81,64 @@ def registerpackage():
             elif not chip_number:
                 error = "Chip number is required."
             elif not plant_id:
-                plant_id = cursor.execute("SELECT plant_id From Machine WHERE status = %s", "finished").fetchone()
-            
+                cursor.execute("SELECT plant_id From Machine WHERE status = %s", "finished")
+                plt =  cursor.fetchone()
+                plant_id = plt[0]
             if error is not None:
                 flash(error)
             else:
-                price = cursor.execute(
-                    "SELECT price From Chip_expense WHERE chip_type = %s", chip_type
-                    ).fetchone()
-                cursor.execute(
-                    "INSERT INTO Package(package_id, chip_number, chip_type, plant_id, consumer_id, price) VALUES (%s, %d, %s, %s, %s)",(package_id,chip_number,chip_type, plant_id,g.user['consumer_id'], price)
-                    ) 
-                db.commit()  
-                return redirect(url_for('consumer.payment'))
+                cursor.execute("SELECT price From Chip_expense WHERE chip_type = %s", chip_type)
+                price = cursor.fetchone()
+                cursor.execute("SELECT balance FROM Consumer WHERE consumer_id = %s", g.user)
+                balance = cursor.fetchone()
+                package_info = []
+                package_info.append(package_id)
+                package_info.append(chip_number)
+                package_info.append(chip_type)
+                package_info.append(plant_id)
+                package_info.append(1.2*price[0]*int(chip_number))   
+                package_info.append(balance[0])   
+                return render_template('payment.html', package = package_info)          
+        return render_template('index_consumer.html', plant_id = plant_id, chip_type = chip_type, error = error)
 
-   
-@bp.route('/payment')
+@bp.route('/payment/<package>', methods=('GET', 'POST'))
 @login_required
-def payment():
+def payment(package):
+    error = None
     if (g.user):
         db = get_db()
-        cursor = db.cursor()
-        error = None
-        cursor.execute("SELECT balance FROM Consumer WHERE consumer_id = %s", g.user["consumer_id"])
+        cursor = db.cursor()  
+        success = False   
+        cursor.execute("SELECT balance FROM Consumer WHERE consumer_id = %s", g.user)
         balance = cursor.fetchone()
-        cursor.execute("SELECT price FROM Package WHERE package_id = %s", g.user["package_id"])
-        price = cursor.fetchone()
-        if balance - price > 0:
-            cursor.execute("UPDATE FROM Consumer SET balance = %f WHERE consumer_id = %s", (balance-price, g.user["consumer_id"]))
+        package = package.replace('\'', '')
+        package = package.replace('[', '')
+        package = package.replace(']', '')
+        info = package.split(',')
+        price = float(info[4])
+        if balance[0] - price >= 0:      
+            cursor.execute(
+                "INSERT INTO Packages(package_id, chip_number, chip_type, plant_id, consumer_id, total_expense, price) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (int(info[0]),int(info[1]),info[2], int(info[3]),g.user, info[4], price*1.2))  
+            cursor.execute("UPDATE Consumer SET balance = %s WHERE consumer_id = %s", (balance[0]-price, g.user))
             db.commit()
-            print("Payment is successful. Your package id is:",g.user["package_id"])
-            return redirect(url_for('consumer.index_consumer'))
+            success = True
+            print("Payment is successful. Your package id is:",package[0])
+            if request.method == "POST":
+                info[5] = balance[0]-price
+                return render_template('payment.html',  package = info, success = success, error = error)
         else: 
             error = "Your balance is not available, payment fails."
-        print("Your payment error is:", error)
-        flash(error)
-        return render_template('payment.html', error = error)
-    return render_template('payment.html')
+            print("Your payment error is:", error)
+            flash(error)
+            return render_template('payment.html', package = info, success = success, error = error)
+    return render_template('payment.html', package = info, success = success, error = error)
 
-# def return_button():
-#     return redirect(url_for('index_consumer'))
+@bp.route('/payreturn', methods=('GET', 'POST'))
+@login_required
+def payreturn():
+
+    return redirect(url_for('consumer.index_consumer'))
+
+
         
